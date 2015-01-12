f76d51912d5d1c2d213163f9488767c3bd09e096
Trevor Bača <trevorbaca@gmail.com>
2015-01-12 15:37:03 -0500

    Added top-level directories.

---
+++ __metadata__.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+import collections
+
+
+metadata = collections.OrderedDict([
+    ('catalog_number', 'AWN-015'),
+    ('forces_tagline', 'for ten players'),
+    ('paper_dimensions', '17 x 11 in'),
+    ('price', '\\euro 66 / \\$ 84'),
+    ('title', 'Al-kitab al-khamr'),
+    ('year', 2015),
+    ])
