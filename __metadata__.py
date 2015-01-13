74ce186f199057287a065bbd957d44a9e8c7eba8
Trevor Bača <trevorbaca@gmail.com>
2015-01-13 16:07:50 -0500

    Updated tagline.

--- __metadata__.py
+++ __metadata__.py
@@ -4,7 +4,7 @@ import collections
 
 metadata = collections.OrderedDict([
     ('catalog_number', 'AWN-015'),
-    ('forces_tagline', 'for ten players'),
+    ('forces_tagline', 'for eleven players'),
     ('paper_dimensions', '17 x 11 in'),
     ('price', '\\euro 66 / \\$ 84'),
     ('title', 'Al-kitab al-khamr'),
