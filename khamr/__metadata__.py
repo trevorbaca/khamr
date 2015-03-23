4e9a70ad3b3a24fb2a8a50d348dd751b38eae1b3
Trevor Bača <trevorbaca@gmail.com>
2015-03-23 18:10:55 -0400

    Changed to portrait layout.

--- khamr/__metadata__.py
+++ khamr/__metadata__.py
@@ -4,8 +4,8 @@ import collections
 
 metadata = collections.OrderedDict([
     ('catalog_number', 'AWN-015'),
-    ('forces_tagline', 'for eleven players'),
-    ('paper_dimensions', '17 x 11 in'),
+    ('forces_tagline', 'for chamber orchestra'),
+    ('paper_dimensions', '11 x 17 in'),
     ('price', '\\euro 66 / \\$ 84'),
     ('title', 'Al-kitab al-khamr'),
     ('year', 2015),
