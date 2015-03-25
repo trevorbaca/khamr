e982b56baf405acbe12c92d0801df4039d390a3a
Trevor Bača <trevorbaca@gmail.com>
2015-03-25 16:46:37 -0400

    Built time signatures.

--- khamr/materials/miscellaneous.py
+++ khamr/materials/miscellaneous.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+import baca
 from abjad import *
 
 
@@ -34,7 +35,21 @@ tempo_inventory = {
     126: indicatortools.Tempo(Duration(1, 4), 126),
     }
 
+# 108 seconds / segment
+#   = 226.8 beats at 126 MM
+#   = 151.2 beats at 84 MM
+#   = 113.4 beats at 63 MM
+#   = 75.6 beats at 42 MM
+numerators = baca.utilities.helianthate(
+    [[2, 2, 3], [2, 4], [3, 4, 5]],
+    -1,
+    -1
+    )
+numerators = sequencetools.flatten_sequence(numerators)
+time_signatures = [TimeSignature((_, 4)) for _ in numerators]
+
 __all__ = [
     'metric_modulation_inventory',
     'tempo_inventory',
+    'time_signatures',
     ]
