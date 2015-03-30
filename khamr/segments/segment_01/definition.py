e8657578990a64082c97f4f4ce3fbe7f1901d11f
Trevor Bača <trevorbaca@gmail.com>
2015-03-30 16:37:40 -0400

    Reworked [1], [2], [3], [4], [5].

--- khamr/segments/segment_01/definition.py
+++ khamr/segments/segment_01/definition.py
@@ -2,34 +2,29 @@
 from abjad import *
 from abjad.tools.rhythmmakertools import BooleanPattern
 from experimental import *
-from khamr import makers
-from khamr import materials
-from khamr.materials.abbreviations import *
+import khamr
 
 
-### INITIALIZATION ##
+###############################################################################
+############################## SEGMENT-MAKER ##################################
+###############################################################################
 
-segment_maker = makers.SegmentMaker(
+segment_maker = khamr.makers.SegmentMaker(
+    measures_per_stage=[
+        2, 2, 2,                    # dummy stages 1-3 
+        ],
     name='A',
     show_stage_annotations=True,
+    tempo_map = [
+        (1, khamr.materials.tempi[126]),
+        ],
+    time_signatures=khamr.materials.time_signatures[:6],
     transpose_score=True,
     )
 
-### STAGES ###
-segment_maker.time_signatures = materials.time_signatures[:6]
-segment_maker.measures_per_stage = [
-    2, 2, 2,                    # dummy stages 1-3 
-    ]
-measure_count = sum(segment_maker.measures_per_stage)
 assert segment_maker.measure_count == 6
 assert segment_maker.stage_count == 3
 assert segment_maker.validate_time_signatures()
-
-### TEMPO MAP ###
-music_maker = segment_maker.make_music_maker()
-segment_maker.tempo_map = [
-    (1, materials.tempi[126]),
-    ]
 
 ###############################################################################
 ################################ MUSIC-MAKERS #################################
