9ad815f8f3a99cadbad6ef0b7605c1d356cff710
Trevor Bača <trevorbaca@gmail.com>
2015-03-31 15:25:41 -0400

    Updated parameterzied segment tests.

--- khamr/test/test_khamr_segments.py
+++ khamr/test/test_khamr_segments.py
@@ -5,19 +5,16 @@ import pytest
 import shutil
 import khamr
 from abjad.tools import systemtools
+abjad_ide = ide.idetools.AbjadIDE(is_test=True)
 
 
 boilerplate_path = ide.idetools.Configuration().boilerplate_directory
 boilerplate_path = os.path.join(boilerplate_path, '__illustrate_segment__.py')
-
 segments_path = os.path.join(khamr.__path__[0], 'segments')
-
 directory_names = os.listdir(segments_path)
 directory_names = [_ for _ in directory_names if not _.startswith(('.', '_'))]
-
 segment_paths = [os.path.join(segments_path, _) for _ in directory_names]
 segment_paths = [_ for _ in segment_paths if os.path.isdir(_)]
-
 
 @pytest.mark.parametrize('segment_path', segment_paths)
 def test_khamr_segments_01(segment_path):
@@ -48,16 +45,18 @@ def test_khamr_segments_01(segment_path):
         remove=[local_boilerplate_path],
         ):
         shutil.copyfile(boilerplate_path, local_boilerplate_path)
+        abjad_ide._replace_in_file(
+            local_boilerplate_path,
+            'PREVIOUS_SEGMENT_METADATA_IMPORT_STATEMENT',
+            'previous_segment_metadata = None',
+            )
         assert os.path.exists(local_boilerplate_path)
         assert not os.path.exists(illustration_candidate_ly_path)
         assert not os.path.exists(illustration_candidate_pdf_path)
-
         command = 'python {}'.format(local_boilerplate_path)
         exit_status = systemtools.IOManager.spawn_subprocess(command)
         assert exit_status == 0
-
         assert os.path.exists(illustration_candidate_ly_path)
         assert os.path.exists(illustration_candidate_pdf_path)
-
         shutil.move(illustration_candidate_ly_path, illustration_ly_path)
         shutil.move(illustration_candidate_pdf_path, illustration_pdf_path)
