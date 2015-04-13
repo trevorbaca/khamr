45b32bafeb56c3a11f568f521e7b77f60805f199
Trevor Bača <trevorbaca@gmail.com>
2015-04-13 10:50:58 -0400

    Reworked [1].

--- khamr/segments/segment_01/definition.py
+++ khamr/segments/segment_01/definition.py
@@ -36,17 +36,163 @@ assert segment_maker.validate_time_signatures()
 ################################ MUSIC-MAKERS #################################
 ###############################################################################
 
+### FLUTE ###
 
-#segment_maker.make_music_maker(
-#    stages=(3, 9),
-#    context_name=khamr.materials.context_names['flute'],
-#    instrument=khamr.materials.instruments['bass flute'],
-#    rhythm_maker=baca.materials.tied_notes,
-#    )
-#
-#segment_maker.make_music_maker(
-#    stages=(1, 2),
-#    context_name=khamr.materials.context_names['cello'],
-#    rhythm_maker=baca.materials.tied_notes,
-#    clef='tenor',
-#    )
+segment_maker.make_music_maker(
+    stages=(1, 7),
+    context_name=fl,
+    instrument=khamr.materials.instruments['bass flute'],
+    division_maker=beat_division_maker
+        .fuse_by_counts(
+            counts=[10, 10, 6, 10, 8, 6],
+            )
+        ,
+    rewrite_meter=True,
+    rhythm_maker=rhythmmakertools.IncisedRhythmMaker(
+        incise_specifier=rhythmmakertools.InciseSpecifier(
+            prefix_talea=[-1],
+            prefix_counts=[0],
+            suffix_talea=[-1],
+            suffix_counts=[1],
+            talea_denominator=8,
+            ),
+        output_masks=[
+            rhythmmakertools.silence_every([2, 5], period=6),
+            ],
+        tie_specifier=rhythmmakertools.TieSpecifier(
+            use_messiaen_style_ties=True,
+            ),
+        ),
+    )
+
+### OBOE ###
+
+segment_maker.make_music_maker(
+    stages=(1, 7),
+    context_name=ob,
+    instrument=khamr.materials.instruments['English horn'],
+    division_maker=beat_division_maker
+        .fuse_by_counts(
+            counts=[12, 6, 10, 10, 6, 8],
+            )
+        ,
+    rewrite_meter=True,
+    rhythm_maker=rhythmmakertools.IncisedRhythmMaker(
+        incise_specifier=rhythmmakertools.InciseSpecifier(
+            prefix_talea=[-1],
+            prefix_counts=[0],
+            suffix_talea=[-1],
+            suffix_counts=[1],
+            talea_denominator=8,
+            ),
+        output_masks=[
+            rhythmmakertools.silence_every([1, 4], period=6),
+            ],
+        tie_specifier=rhythmmakertools.TieSpecifier(
+            use_messiaen_style_ties=True,
+            ),
+        ),
+    )
+
+### CLARINET ###
+
+segment_maker.make_music_maker(
+    stages=(1, 7),
+    context_name=cl,
+    instrument=khamr.materials.instruments['bass clarinet'],
+    division_maker=beat_division_maker
+        .fuse_by_counts(
+            counts=[8, 6, 10, 6, 10, 8],
+            )
+        ,
+    rewrite_meter=True,
+    rhythm_maker=rhythmmakertools.IncisedRhythmMaker(
+        incise_specifier=rhythmmakertools.InciseSpecifier(
+            prefix_talea=[-1],
+            prefix_counts=[0],
+            suffix_talea=[-1],
+            suffix_counts=[1],
+            talea_denominator=8,
+            ),
+        output_masks=[
+            rhythmmakertools.silence_every([1, 3], period=6),
+            ],
+        tie_specifier=rhythmmakertools.TieSpecifier(
+            use_messiaen_style_ties=True,
+            ),
+        ),
+    )
+
+### SAXOPHONE ###
+
+segment_maker.make_music_maker(
+    stages=(1, 7),
+    context_name=sax,
+    instrument=khamr.materials.instruments['baritone saxophone'],
+    division_maker=beat_division_maker
+        .fuse_by_counts(
+            counts=[14, 6, 10, 6, 10, 8],
+            )
+        ,
+    rewrite_meter=True,
+    rhythm_maker=rhythmmakertools.IncisedRhythmMaker(
+        incise_specifier=rhythmmakertools.InciseSpecifier(
+            prefix_talea=[-1],
+            prefix_counts=[0],
+            suffix_talea=[-1],
+            suffix_counts=[1],
+            talea_denominator=8,
+            ),
+        output_masks=[
+            rhythmmakertools.silence_every([1, 3], period=6),
+            ],
+        tie_specifier=rhythmmakertools.TieSpecifier(
+            use_messiaen_style_ties=True,
+            ),
+        ),
+    )
+
+### GUITAR ###
+
+### PIANO ###
+
+### PERCUSSION ###
+
+### VIOLIN ###
+
+### VIOLA ###
+
+### CELLO ###
+
+### CONTRABASS ###
+
+
+
+
+
+
+###############################################################################
+############################### MUSIC-HANDLERS ################################
+###############################################################################
+
+### FLUTE ###
+
+### OBOE ###
+
+### CLARINET ###
+
+### SAXOPHONE ###
+
+### GUITAR ###
+
+### PIANO ###
+
+### PERCUSSION ###
+
+### VIOLIN ###
+
+### VIOLA ###
+
+### CELLO ###
+
+### CONTRABASS ###
