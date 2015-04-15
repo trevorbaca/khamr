8825aa8980cc61835fbe3601f0daad7b11765e39
Trevor Bača <trevorbaca@gmail.com>
2015-04-14 22:02:21 -0400

    Built halo pitches.

---
+++ khamr/materials/halo_pitches/output.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools import datastructuretools
+from abjad.tools import pitchtools
+
+
+halo_pitches = datastructuretools.CyclicTuple(
+    [
+        pitchtools.NamedPitch('a,'),
+        pitchtools.NamedPitch('bf,'),
+        pitchtools.NamedPitch('aqs,'),
+        pitchtools.NamedPitch('b,'),
+        pitchtools.NamedPitch('cqs'),
+        pitchtools.NamedPitch('bf,'),
+        pitchtools.NamedPitch('aqs,'),
+        pitchtools.NamedPitch('b,'),
+        pitchtools.NamedPitch('a,'),
+        pitchtools.NamedPitch('b,'),
+        pitchtools.NamedPitch('cs'),
+        pitchtools.NamedPitch('bqs,'),
+        pitchtools.NamedPitch('c'),
+        pitchtools.NamedPitch('a,'),
+        pitchtools.NamedPitch('b,'),
+        pitchtools.NamedPitch('cs'),
+        pitchtools.NamedPitch('bqs,'),
+        pitchtools.NamedPitch('dqf'),
+        pitchtools.NamedPitch('c'),
+        pitchtools.NamedPitch('d'),
+        pitchtools.NamedPitch('a,'),
+        pitchtools.NamedPitch('d'),
+        pitchtools.NamedPitch('cqs'),
+        pitchtools.NamedPitch('dqs'),
+        pitchtools.NamedPitch('e'),
+        pitchtools.NamedPitch('cs'),
+        pitchtools.NamedPitch('d'),
+        pitchtools.NamedPitch('eqf'),
+        pitchtools.NamedPitch('f'),
+        pitchtools.NamedPitch('d'),
+        pitchtools.NamedPitch('eqf'),
+        pitchtools.NamedPitch('fqf'),
+        pitchtools.NamedPitch('e'),
+        ]
+    )
