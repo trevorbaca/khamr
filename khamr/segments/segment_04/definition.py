01232023e4bb5eb22ed5958e14cb53bcf223d1aa
Trevor Bača <trevorbaca@gmail.com>
2015-04-16 19:13:16 -0400

    Reworked [4].

--- khamr/segments/segment_04/definition.py
+++ khamr/segments/segment_04/definition.py
@@ -146,20 +146,22 @@ segment_maker.make_music_maker(
 segment_maker.make_music_maker(
     stages=(8, 8),
     context_name=gt,
-    rewrite_meter=True,
     division_maker=makertools.DivisionMaker()
         .fuse_by_counts(
             counts=mathtools.Infinity,
             )
         .split_by_durations(
-            durations=[(4, 4)],
+            durations=[(4, 4), (12, 4)],
             )
         ,
+    rewrite_meter=True,
     rhythm_maker=rhythmmakertools.NoteRhythmMaker(
         output_masks=[
-            rhythmmakertools.silence_all(),
-            rhythmmakertools.sustain_first(2),
+            rhythmmakertools.silence_last(1),
             ],
+        tie_specifier=rhythmmakertools.TieSpecifier(
+            use_messiaen_style_ties=True,
+            )
         ),
     )
 
@@ -192,20 +194,22 @@ segment_maker.make_music_maker(
 segment_maker.make_music_maker(
     stages=(8, 8),
     context_name=pf,
-    rewrite_meter=True,
     division_maker=makertools.DivisionMaker()
         .fuse_by_counts(
             counts=mathtools.Infinity,
             )
         .split_by_durations(
-            durations=[(4, 4)],
+            durations=[(4, 4), (12, 4)],
             )
         ,
+    rewrite_meter=True,
     rhythm_maker=rhythmmakertools.NoteRhythmMaker(
         output_masks=[
-            rhythmmakertools.silence_all(),
-            rhythmmakertools.sustain_first(2),
+            rhythmmakertools.silence_last(1),
             ],
+        tie_specifier=rhythmmakertools.TieSpecifier(
+            use_messiaen_style_ties=True,
+            )
         ),
     )
 
@@ -246,15 +250,68 @@ segment_maker.make_music_maker(
 
 ### FLUTE HANDLERS ###
 
+segment_maker.make_music_handler(
+    scope=(fl, (1, 7)),
+    specifiers=[
+        # sounds B3
+        pitch_specifier(source='Bb4'),
+        ],
+    )
+
 ### OBOE HANDLERS ###
+
+segment_maker.make_music_handler(
+    scope=(ob, (1, 7)),
+    specifiers=[
+        # dummy centerline pitch
+        pitch_specifier(source='B4'),
+        single_line_staff,
+        repeat_tie_up,
+        ],
+    )
 
 ### CLARINET HANDLERS ###
 
+segment_maker.make_music_handler(
+    scope=(cl, (1, 7)),
+    specifiers=[
+        pitch_specifier(source='G2'),
+        ]
+    )
+
 ### SAXOPHONE HANDLERS ###
+
+segment_maker.make_music_handler(
+    scope=(sax, (1, 7)),
+    specifiers=[
+        # dummy centerline pitch
+        pitch_specifier(source='D5'),
+        single_line_staff,
+        repeat_tie_up,
+        ],
+    )
 
 ### GUITAR HANDLERS ##
 
+segment_maker.make_music_handler(
+    scope=(gt, (1, 8)),
+    specifiers=[
+        pitch_specifier(source='C4'),
+        reiterated_flageolets,
+        reiterated_mf,
+        ],
+    )
+
 ### PIANO HANDLERS ###
+
+segment_maker.make_music_handler(
+    scope=(pf, (1, 8)),
+    specifiers=[
+        pitch_specifier(source='A#4'),
+        reiterated_flageolets,
+        reiterated_mf,
+        ],
+    )
 
 ### PERCUSSION HANDLERS ###
 
@@ -270,6 +327,7 @@ segment_maker.make_music_handler(
     specifiers=[
         Dynamic('fff'),
         stem_tremolo,
+        percussion_reminder_markup('snare drum'),
         ],
     )
 
