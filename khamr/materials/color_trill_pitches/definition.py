b086ab3ad186794531c8f754012ed419b33fe13c
Trevor Bača <trevorbaca@gmail.com>
2015-04-16 08:39:09 -0400

    Built color trill pitches.

---
+++ khamr/materials/color_trill_pitches/definition.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import baca
+
+
+color_trill_pitches = r''' 
+    F4 F#4 F+4 F4 F+4 F4 E4 E+4 E4 E+4 F4 F+4 F#4 F+4 F#4 F#+4 F#4 E+4
+    '''
+color_trill_pitches = color_trill_pitches.split()
+color_trill_pitches = [NamedPitch(_) for _ in color_trill_pitches]
+assert len(color_trill_pitches) == 18
