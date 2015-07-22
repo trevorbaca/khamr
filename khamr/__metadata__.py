85990c67f112bbf3bb984300fc2fd41a0d12b430
Trevor Bača <trevorbaca@gmail.com>
2015-07-22 14:20:21 -0400

    Updated metadata.

--- khamr/__metadata__.py
+++ khamr/__metadata__.py
@@ -4,7 +4,7 @@ import collections
 
 metadata = collections.OrderedDict([
     ('catalog_number', 'AWN-015'),
-    ('forces_tagline', 'for chamber orchestra'),
+    ('forces_tagline', 'for eleven players'),
     ('paper_dimensions', '11 x 17 in'),
     ('price', '\\euro 66 / \\$ 84'),
     ('title', 'Al-kitab al-khamr'),
