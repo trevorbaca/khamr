a75a09dd80b9b9d5d3494714e0b70c9b79664736
Trevor Bača <trevorbaca@gmail.com>
2015-09-12 14:26:27 -0400

    Updated tests.

--- khamr/test/illustrate_segments.py
+++ khamr/test/illustrate_segments.py
@@ -1,30 +1,19 @@
 # -*- coding: utf-8 -*-
-import os
-import pytest
 import shutil
-import khamr
+import os
+import subprocess
 import ide
 from abjad.tools import systemtools
-pytest.skip('possibly deprecated in favor of test/illustrate_segments.py')
 
 
 configuration = ide.tools.idetools.AbjadIDEConfiguration()
 boilerplate_path = configuration.abjad_ide_boilerplate_directory
 boilerplate_path = os.path.join(boilerplate_path, '__illustrate_segment__.py')
 
-segments_path = os.path.join(khamr.__path__[0], 'segments')
-
-directory_names = os.listdir(segments_path)
-directory_names = [_ for _ in directory_names if not _.startswith(('.', '_'))]
-
-segment_paths = [os.path.join(segments_path, _) for _ in directory_names]
-segment_paths = [_ for _ in segment_paths if os.path.isdir(_)]
-
-@pytest.mark.parametrize('segment_path', segment_paths)
-def test_segments_01(segment_path):
-    r'''Illustrates segments.
-    '''
-    
+def illustrate_segment(segment_path):
+    message = 'Illustrating {} ...'
+    message = message.format(segment_path)
+    print(message)
     local_boilerplate_path = os.path.join(
         segment_path,
         '__illustrate_segment__.py',
@@ -45,10 +34,8 @@ def test_segments_01(segment_path):
         segment_path,
         'illustration.candidate.pdf',
         )
-
     if os.path.exists(local_boilerplate_path):
         os.remove(local_boilerplate_path)
-
     with systemtools.FilesystemState(
         keep=[illustration_ly_path, illustration_pdf_path],
         remove=[local_boilerplate_path],
@@ -69,3 +56,26 @@ def test_segments_01(segment_path):
         assert os.path.exists(illustration_candidate_pdf_path)
         shutil.move(illustration_candidate_ly_path, illustration_ly_path)
         shutil.move(illustration_candidate_pdf_path, illustration_pdf_path)
+
+
+if __name__ == '__main__':
+    this_file = os.path.abspath(__file__)
+    test_directory = os.path.dirname(this_file)
+    inner_score_directory = os.path.dirname(test_directory)
+    segments_directory = os.path.join(
+        inner_score_directory,
+        'segments',
+        )
+    for name in os.listdir(segments_directory):
+        segment_directory = os.path.join(
+            segments_directory,
+            name,
+            )
+        definition_file = os.path.join(
+            segments_directory,
+            name,
+            'definition.py',
+            )
+        if not os.path.isfile(definition_file):
+            continue
+        illustrate_segment(segment_directory)
