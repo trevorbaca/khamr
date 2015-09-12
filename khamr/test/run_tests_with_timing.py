73e1bc77873f58f272f3e73eab25effd1061f84d
Trevor Bača <trevorbaca@gmail.com>
2015-09-12 12:58:55 -0400

    Updated tests.

--- khamr/test/run_tests_with_timing.py
+++ khamr/test/run_tests_with_timing.py
@@ -9,5 +9,9 @@ if __name__ == '__main__':
     this_test_directory = os.path.dirname(this_file)
     command = 'py.test -xrf {}'
     command = command.format(this_test_directory)
-    with systemtools.Timer(print_continuously_from_background=10):
+    with systemtools.Timer(print_continuously_from_background=30):
         exit_code = subprocess.call(command, shell=True)
+    if not exit_code == 0:
+        message = 'pytest failed with exit code {}.'
+        message = message.format(exit_code)
+        raise Exception(message)
