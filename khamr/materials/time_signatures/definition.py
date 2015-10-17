5c60c5c1d8ed9d8620618f4fb9d2943cb48e68a3
Trevor Bača <trevorbaca@gmail.com>
2015-10-17 18:03:40 -0400

    Changed utilities to tools.

--- khamr/materials/time_signatures/definition.py
+++ khamr/materials/time_signatures/definition.py
@@ -8,12 +8,12 @@ from abjad import *
 #   = 151.2 beats at 84 MM
 #   = 113.4 beats at 63 MM
 #   = 75.6 beats at 42 MM
-numerators = baca.utilities.helianthate(
+numerators = baca.tools.helianthate(
     [[2, 2, 3], [2, 4], [3, 4, 5]],
     -1,
     -1
     )
-pairs = baca.utilities.helianthate(
+pairs = baca.tools.helianthate(
     [[(2, 4), (2, 4), (6, 4)], [(3, 4), (4, 4)], [(6, 8), (4, 4), (5, 4)]],
     -1,
     -1
