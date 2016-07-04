780f14d9010c5719f58183fb38e1a4461ec2d702
Trevor Bača <trevor.baca@gmail.com>
2016-07-04 11:17:20 -0500

    Ported specifiers forward.

--- khamr/segments/segment_03/definition.py
+++ khamr/segments/segment_03/definition.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from abjad import *
-from experimental import *
+import abjad
 import baca
 import khamr
 from khamr.materials.__abbreviations__ import *
@@ -523,10 +522,8 @@ segment_maker.make_scoped_specifiers(
         pitch_specifier(
             source='D6 E6 F#6 C6 C#6 D6 D#6 F6',
             ),
-        handlertools.PatternedArticulationsHandler(
-            articulation_lists=[
-                ['>'], None, None, None, None, None, None, None,
-                ],
+        baca.tools.ArticulationSpecifier(
+            articulations=['>', None, None, None, None, None, None, None],
             ),
         ],
     )
@@ -559,10 +556,8 @@ segment_maker.make_scoped_specifiers(
         pitch_specifier(
             source='C6 C#6 D#6 E6 F6 F#6',
             ),
-        handlertools.PatternedArticulationsHandler(
-            articulation_lists=[
-                ['>'], None, None, None, None, None,
-                ],
+        baca.tools.ArticulationSpecifier(
+            articulations=['>', None, None, None, None, None],
             ),
         ],
     )
@@ -595,9 +590,9 @@ segment_maker.make_scoped_specifiers(
         pitch_specifier(
             source='D6 D#6 F6 F#6 C6 C#6 D#6 E6 F6',
             ),
-        handlertools.PatternedArticulationsHandler(
-            articulation_lists=[
-                ['>'], None, None, None, None, None, None, None, None,
+        baca.tools.ArticulationSpecifier(
+            articulations=[
+                '>', None, None, None, None, None, None, None, None,
                 ],
             ),
         ],
@@ -629,10 +624,8 @@ segment_maker.make_scoped_specifiers(
         pitch_specifier(
             source='C6 C#6 D6 F6 F#6 D#6 E6',
             ),
-        handlertools.PatternedArticulationsHandler(
-            articulation_lists=[
-                ['>'], None, None, None, None, None, None,
-                ],
+        baca.tools.ArticulationSpecifier(
+            articulations=['>', None, None, None, None, None, None],
             ),
         ],
     )
@@ -699,9 +692,9 @@ segment_maker.make_scoped_specifiers(
         pitch_specifier(
             source='C6 D6 D#6 E6 F6 F#6 C6 C#6 D#6 E6 F6',
             ),
-        handlertools.PatternedArticulationsHandler(
-            articulation_lists=[
-                ['>'], 
+        baca.tools.ArticulationSpecifier(
+            articulations=[
+                '>', 
                 None, None, None, None, None, None, None, None, None, None,
                 ],
             ),
