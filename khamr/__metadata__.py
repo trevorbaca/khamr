490675d579651820029ef2f3b9a43bc405cab7da
Trevor Bača <trevor.baca@gmail.com>
2016-12-10 14:38:06 -0600

    Cleaned up import statements.

--- khamr/__metadata__.py
+++ khamr/__metadata__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-import collections
+import abjad
 
 
-metadata = collections.OrderedDict([
+metadata = abjad.datastructuretools.TypedOrderedDict([
     ('catalog_number', 'AWN-011'),
     ('forces_tagline', 'for eleven players'),
     ('paper_size', '11 x 17 in'),
--- khamr/tools/SegmentMaker.py
+++ khamr/tools/SegmentMaker.py
@@ -2,7 +2,6 @@
 from __future__ import print_function
 import abjad
 import baca
-import collections
 import copy
 import experimental
 import os
