a47a220ca9d33ce771c1f7bfd4597704f338b339
Trevor Bača <trevor.baca@gmail.com>
2017-01-07 12:25:42 -0600

    Modernized import statements.

--- khamr/tools/__init__.py
+++ khamr/tools/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-from abjad.tools import systemtools
+import abjad
 
-systemtools.ImportManager.import_structured_package(
+
+abjad.systemtools.ImportManager.import_structured_package(
     __path__[0],
     globals(),
     )
