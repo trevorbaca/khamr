302f880a04a2339e08f920a96d16d5ab5fe9ee29
Trevor Bača <trevor.baca@gmail.com>
2017-04-04 16:16:22 -0500

    Updated pitch references.

--- khamr/tools/make_narrow_sixth_octave.py
+++ khamr/tools/make_narrow_sixth_octave.py
@@ -7,7 +7,7 @@ def make_narrow_sixth_octave():
     r'''Makes narrow sixth octave register command.
     '''
     return baca.tools.RegisterCommand(
-        registration=abjad.pitchtools.Registration(
+        registration=abjad.Registration(
             [('[A0, F#4)', 22), ('[F#4, C8]', 25)],
             ),
         )
--- khamr/tools/make_wide_third_octave.py
+++ khamr/tools/make_wide_third_octave.py
@@ -7,7 +7,7 @@ def make_wide_third_octave():
     r'''Makes wide third octave register command.
     '''
     return baca.tools.RegisterCommand(
-        registration=abjad.pitchtools.Registration(
+        registration=abjad.Registration(
             [('[A0, F#4)', -20), ('[F#4, C8]', -6)]
             ),
         )
