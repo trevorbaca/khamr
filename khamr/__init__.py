16f2207e7e68e99f674cccd89934f87f19c9cae7
Trevor Bača <trevor.baca@gmail.com>
2017-04-29 19:51:10 -0500

    Cleaned up package initializer.

--- khamr/__init__.py
+++ khamr/__init__.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+import distutils.version
+import platform
+if not (
+    distutils.version.LooseVersion('3.5') <
+    distutils.version.LooseVersion(platform.python_version())
+    ):
+    raise ImportError('Requires Python 3.5.')
+del distutils
+del platform
 from khamr import tools
 from khamr.tools import MarkupLibrary as markup
 from khamr import materials
