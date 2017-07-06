3554f05108ddb1a13608aaa28d9ce23ca0382c32
Trevor Bača <trevor.baca@gmail.com>
2017-07-06 09:28:54 -0500

    Changed Tempo to MetronomeMark.

--- khamr/materials/tempi/definition.py
+++ khamr/materials/tempi/definition.py
@@ -5,22 +5,22 @@ import abjad
 tempi = abjad.TypedOrderedDict([
     (
         32,
-        abjad.Tempo(abjad.Duration(1, 4), 32),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 32),
         ),
     (
         42,
-        abjad.Tempo(abjad.Duration(1, 4), 42),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 42),
         ),
     (
         63,
-        abjad.Tempo(abjad.Duration(1, 4), 63),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 63),
         ),
     (
         84,
-        abjad.Tempo(abjad.Duration(1, 4), 84),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 84),
         ),
     (
         126,
-        abjad.Tempo(abjad.Duration(1, 4), 126),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 126),
         ),
     ])
