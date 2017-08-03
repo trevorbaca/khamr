27bd786f328ca4564b0bd9ad6f3c0ce15e5d7eb7
Trevor Bača <trevor.baca@gmail.com>
2017-08-03 17:26:02 -0500

    Updated test_materials.py, test_segments.py files.

--- khamr/test/test_materials.py
+++ khamr/test/test_materials.py
@@ -1,43 +1,23 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
-import abjad
 import ide
 import os
 import pytest
 import sys
-import traceback
+
+
 abjad_ide = ide.tools.idetools.AbjadIDE()
-
-
-this_file = os.path.abspath(__file__)
-test_directory = os.path.dirname(this_file)
-inner_score_directory = os.path.dirname(test_directory)
-outer_score_directory = os.path.dirname(inner_score_directory)
-composer_scores_directory = os.path.dirname(outer_score_directory)
-# Travis monkey patch
-abjad_ide._configuration._composer_scores_directory_override = \
-    composer_scores_directory
-materials_directory = abjad_ide._to_score_directory(this_file, 'materials')
-material_directories = abjad_ide._list_visible_paths(materials_directory)
-
+directories = abjad_ide._configure_travis_tests(os.path.abspath(__file__))
+material_directories, segment_directories = directories
 
 @pytest.mark.parametrize('material_directory', material_directories)
 def test_materials_01(material_directory):
-    r'''Checks material definition files.
-    '''
-    try:
-        abjad_ide.check_definition_file(material_directory)
-    except:
-        traceback.print_exc()
-        sys.exit(1)
+    exit_code_ = abjad_ide.check_definition_file(material_directory)
+    if exit_code_ != 0:
+        sys.exit(exit_code_)
 
 
 @pytest.mark.parametrize('material_directory', material_directories)
 def test_materials_02(material_directory):
-    r'''Makes material PDFs.
-    '''
-    try:
-        abjad_ide.make_pdf(material_directory)
-    except:
-        traceback.print_exc()
-        sys.exit(1)
+    exit_code_ = abjad_ide.make_pdf(material_directory)
+    if exit_code_ != 0:
+        sys.exit(exit_code_)
--- khamr/test/test_segments.py
+++ khamr/test/test_segments.py
@@ -1,43 +1,26 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
+import ide
 import os
 import pytest
 import sys
-import traceback
-import ide
+
+
 abjad_ide = ide.tools.idetools.AbjadIDE()
+directories = abjad_ide._configure_travis_tests(os.path.abspath(__file__))
+material_directories, segment_directories = directories
+
+@pytest.mark.parametrize('segment_directory', segment_directories)
+def test_packages_01(segment_directory):
+    exit_code_ = abjad_ide.check_definition_file(segment_directory)
+    if exit_code_ != 0:
+        sys.exit(exit_code_)
 
 
-if __name__ == '__main__':
-    this_file = os.path.abspath(__file__)
-    test_directory = os.path.dirname(this_file)
-    inner_score_directory = os.path.dirname(test_directory)
-    outer_score_directory = os.path.dirname(inner_score_directory)
-    composer_scores_directory = os.path.dirname(outer_score_directory)
-    # Travis monkey patch
-    abjad_ide._configuration._composer_scores_directory_override = \
-        composer_scores_directory
-    segments_directory = abjad_ide._to_score_directory(this_file, 'segments')
-    segment_directories = abjad_ide._list_visible_paths(segments_directory)
-
-    # not parameterized to print keep-alive message to Travis log
-    for segment_directory in segment_directories:
-        message = 'Checking {} definition file ...'
-        message = message.format(abjad_ide._trim_path(segment_directory))
-        print(message)
-        try:
-            abjad_ide.check_definition_file(segment_directory)
-        except:
-            traceback.print_exc()
-            sys.exit(1)
-
-    # not parameterized to print keep-alive message to Travis log
-    for segment_directory in segment_directories:
-        message = 'Making {} PDF ...'
-        message = message.format(abjad_ide._trim_path(segment_directory))
-        print(message)
-        try:
-            abjad_ide.make_pdf(segment_directory)
-        except:
-            traceback.print_exc()
-            sys.exit(1)
+@pytest.mark.parametrize('segment_directory', segment_directories)
+def test_packages_02(segment_directory):
+    # only run on Travis because segment illustration usually takes a while
+    if not os.getenv('TRAVIS'):
+        return
+    exit_code_ = abjad_ide.make_pdf(segment_directory)
+    if exit_code_ != 0:
+        sys.exit(exit_code_)
