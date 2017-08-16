79f65ab4b6912da27134148e3aa7c3891d875c07
Trevor Bača <trevor.baca@gmail.com>
2017-08-16 11:32:07 -0500

    Cleaned up metadat.

--- khamr/__metadata__.py
+++ khamr/__metadata__.py
@@ -3,9 +3,8 @@ import abjad
 
 metadata = abjad.TypedOrderedDict([
     ('catalog_number', 'AWN-011'),
+    ('composer', 'Trevor Bača'),
     ('forces_tagline', 'for eleven players'),
-    ('paper_size', '11 x 17 in'),
-    ('price', '\\euro 66 / \\$ 84'),
     ('title', 'Al-kitab al-khamr'),
     ('year', 2015),
     ])
