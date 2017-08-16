513ca5e2265c80cb427904e9b670f497b82049f3
Trevor Bača <trevor.baca@gmail.com>
2017-08-16 10:19:24 -0500

    Cleaned up setup file.

--- setup.py
+++ setup.py
@@ -1,24 +1,15 @@
-#! /usr/bin/env python
-from distutils.core import setup
+#!/usr/bin/env python
+import setuptools
 
 
-install_requires = (
-    'abjad',
-    )
-
-
-def main():
-    setup(
+if __name__ == '__main__':
+    setuptools.setup(
         author='Trevor Bača',
         author_email='trevor.baca@gmail.com',
-        install_requires=install_requires,
+        install_requires=('abjad',),
         name='khamr',
         packages=('khamr',),
         url='https://github.com/trevorbaca/khamr',
         version='0.1',
         zip_safe=False,
         )
-
-
-if __name__ == '__main__':
-    main()
