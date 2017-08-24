2e83c884b4aa1a7e7c7f1ccef7fb343c1fa82045
Trevor Bača <trevor.baca@gmail.com>
2017-08-24 17:14:35 -0500

    Set Travis tests to 3.6.

--- khamr/__init__.py
+++ khamr/__init__.py
@@ -1,10 +1,10 @@
 import distutils.version
 import platform
 if not (
-    distutils.version.LooseVersion('3.5') <
+    distutils.version.LooseVersion('3.6') <
     distutils.version.LooseVersion(platform.python_version())
     ):
-    raise ImportError('Requires Python 3.5.')
+    raise ImportError('Requires Python 3.6.')
 del distutils
 del platform
 
