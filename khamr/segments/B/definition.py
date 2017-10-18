c632877ab101d18ca1787fbd26f57d1b31064fcc
Trevor Bača <trevor.baca@gmail.com>
2017-10-18 08:22:32 -0500

    Integrated selector changes.

--- khamr/segments/B/definition.py
+++ khamr/segments/B/definition.py
@@ -512,9 +512,7 @@ segment_maker(
     baca.scope('Oboe Music Voice', 5, 6),
     baca.dynamic('fff'),
     baca.pitches('C6 C#6 D#6 E6 F6 F#6'),
-    baca.AttachCommand(
-        arguments=[abjad.Articulation('>'), None, None, None, None, None],
-        ),
+    baca.accents(baca.select().top().tuplets().map(baca.select().leaf(0))),
     )
 
 segment_maker(
@@ -538,12 +536,7 @@ segment_maker(
     baca.scope('Clarinet Music Voice', 5, 6),
     baca.dynamic('fff'),
     baca.pitches('D6 D#6 F6 F#6 C6 C#6 D#6 E6 F6'),
-    baca.AttachCommand(
-        arguments=[
-            abjad.Articulation('>'),
-            None, None, None, None, None, None, None, None,
-            ],
-        ),
+    baca.accents(baca.select().top().tuplets().map(baca.select().leaf(0))),
     )
 
 segment_maker(
@@ -563,10 +556,7 @@ segment_maker(
     baca.scope('Saxophone Music Voice', 5, 6),
     baca.dynamic('fff'),
     baca.pitches('C6 C#6 D6 F6 F#6 D#6 E6'),
-    baca.AttachCommand(
-        arguments=[
-            abjad.Articulation('>'), None, None, None, None, None, None],
-        ),
+    baca.accents(baca.select().top().tuplets().map(baca.select().leaf(0))),
     )
 
 segment_maker(
@@ -618,12 +608,7 @@ segment_maker(
     baca.scope('Piano Music Voice', 5, 6),
     baca.dynamic('fff'),
     baca.pitches('C6 D6 D#6 E6 F6 F#6 C6 C#6 D#6 E6 F6'),
-    baca.AttachCommand(
-        arguments=[
-            abjad.Articulation('>'),
-            None, None, None, None, None, None, None, None, None, None,
-            ],
-        ),
+    baca.accents(baca.select().top().tuplets().map(baca.select().leaf(0))),
     )
 
 ### PERCUSSION ###
