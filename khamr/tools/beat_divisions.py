894a4dd70c861b9c95d057b9139623d6a24b9415
Trevor Bača <trevor.baca@gmail.com>
2017-11-01 13:00:47 -0500

    Integrated flatten changes.

--- khamr/tools/beat_divisions.py
+++ khamr/tools/beat_divisions.py
@@ -10,5 +10,5 @@ def beat_divisions():
         compound_meter_multiplier=abjad.Multiplier(3, 2),
         durations=[(1, 4)],
         )
-    beat_division_maker = beat_division_maker.flatten(depth=-1),
+    beat_division_maker = beat_division_maker.flatten(depth=-1)
     return beat_division_maker
