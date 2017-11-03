e841b1d048509f20956e8a04788546bfa3c9571d
Trevor Bača <trevor.baca@gmail.com>
2017-11-02 20:14:54 -0500

    Integrated hairpin changes.

--- khamr/tools/halo_hairpins.py
+++ khamr/tools/halo_hairpins.py
@@ -2,11 +2,9 @@ import baca
 
 
 def halo_hairpins():
-    r'''
-    '''
-    return baca.hairpins(
-        [
-            'pp > ppp', 'ppp < pp', 'pp > ppp', 'ppp < pp',
-            'pp < p', 'p > pp', 'pp < p', 'p > ppp', 'ppp < pp',
-            ],
-        )
+    hairpins = [
+        'pp > ppp', 'ppp < pp', 'pp > ppp', 'ppp < pp',
+        'pp < p', 'p > pp', 'pp < p', 'p > ppp', 'ppp < pp',
+        ]
+    hairpins = [baca.hairpin(_) for _ in hairpins]
+    return baca.map(hairpins, baca.select().plts())
