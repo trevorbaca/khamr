ad1c9b32d3edb6e653b488338463a8bcfe66fd70
Trevor Bača <trevor.baca@gmail.com>
2017-12-16 16:20:52 -0600

    Cleaned up timing metadata.

--- khamr/segments/B/__metadata__.py
+++ khamr/segments/B/__metadata__.py
@@ -3,6 +3,7 @@ import abjad
 
 metadata = abjad.TypedOrderedDict(
     [
+        ('duration', "1'50''"),
         (
             'end_clefs',
             abjad.TypedOrderedDict(
@@ -54,7 +55,6 @@ metadata = abjad.TypedOrderedDict(
                     ]
                 ),
             ),
-        ('end_clock_time', "5'27''"),
         (
             'end_dynamics',
             abjad.TypedOrderedDict(
@@ -105,6 +105,8 @@ metadata = abjad.TypedOrderedDict(
             ),
         ('first_measure_number', 75),
         ('segment_number', 3),
+        ('start_clock_time', "3'37''"),
+        ('stop_clock_time', "5'27''"),
         (
             'time_signatures',
             [
--- khamr/segments/_/__metadata__.py
+++ khamr/segments/_/__metadata__.py
@@ -3,6 +3,7 @@ import abjad
 
 metadata = abjad.TypedOrderedDict(
     [
+        ('duration', "1'46''"),
         (
             'end_clefs',
             abjad.TypedOrderedDict(
@@ -54,7 +55,6 @@ metadata = abjad.TypedOrderedDict(
                     ]
                 ),
             ),
-        ('end_clock_time', "1'46''"),
         (
             'end_dynamics',
             abjad.TypedOrderedDict(
@@ -103,6 +103,8 @@ metadata = abjad.TypedOrderedDict(
             ),
         ('first_measure_number', 1),
         ('segment_number', 1),
+        ('start_clock_time', "0'00''"),
+        ('stop_clock_time', "1'46''"),
         (
             'time_signatures',
             [
