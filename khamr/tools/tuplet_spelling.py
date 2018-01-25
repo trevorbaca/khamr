63052c10d096ca80537dd7e0697621eb60298791
Trevor Bača <trevor.baca@gmail.com>
2018-01-24 18:20:34 -0600

    Integrated tuplet specifier changes.

--- khamr/tools/tuplet_spelling.py
+++ khamr/tools/tuplet_spelling.py
@@ -5,5 +5,5 @@ def tuplet_spelling():
     r'''Makes tuplet spelling specifier.
     '''
     return rhythmos.TupletSpecifier(
-        flatten_trivial_tuplets=True,
+        extract_trivial=True,
         )
