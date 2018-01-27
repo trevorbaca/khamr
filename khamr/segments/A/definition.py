f576b5334f7f41501a7fb4ad777ce48a3b1d9219
Trevor Bača <trevor.baca@gmail.com>
2018-01-27 15:39:04 -0600

    Integrated stage count validation.

--- khamr/segments/A/definition.py
+++ khamr/segments/A/definition.py
@@ -25,10 +25,8 @@ maker = baca.SegmentMaker(
     time_signatures=khamr.time_signatures[:30],
     transpose_score=True,
     validate_measure_count=30,
+    validate_stage_count=5,
     )
-
-maker.validate_stage_count(5)
-maker.validate_measures_per_stage()
 
 ###############################################################################
 ################################### COMMANDS ##################################
