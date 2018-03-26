034a8c3967c22f08419705f7d23f6abb3d674476
Trevor Bača <trevor.baca@gmail.com>
2018-03-26 11:42:48 -0500

    Cleaning up definition files.

---
+++ khamr/tools/quarter_hits.py
@@ -0,0 +1,27 @@
+import abjad
+import baca
+from abjad import rhythmmakertools as rhythmos
+
+
+def quarter_hits(division_mask=None):
+    r'''Makes quarter hits.
+    '''
+    if division_mask is None:
+        division_masks = None
+    else:
+        division_masks = [division_mask]
+    return baca.RhythmCommand(
+        division_maker=baca.DivisionMaker()
+            .split_by_durations(
+                compound_meter_multiplier=abjad.Multiplier(3, 2),
+                durations=[(1, 4)],
+                )
+            .flatten(depth=-1),
+        rewrite_meter=True,
+        rhythm_maker=rhythmos.NoteRhythmMaker(
+            division_masks=division_masks,
+            tie_specifier=rhythmos.TieSpecifier(
+                repeat_ties=True,
+                ),
+            ),
+        )
