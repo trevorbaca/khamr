755441ef2669c896055e749a59a9d5b7beccc138
Trevor Bača <trevor.baca@gmail.com>
2018-03-26 12:24:06 -0500

    Cleaning up definition files.

--- khamr/tools/__init__.py
+++ khamr/tools/__init__.py
@@ -4,6 +4,7 @@ from .ScoreTemplate import ScoreTemplate
 from .alternate_divisions import alternate_divisions
 from .aviary import aviary
 from .beat_divisions import beat_divisions
+from .closing import closing
 from .continuous_glissandi import continuous_glissandi
 from .current import current
 from .fused_expanse import fused_expanse
