cb4a6cc8559711e36d83d5292211739927364d85
Trevor Bača <trevor.baca@gmail.com>
2018-03-26 11:24:46 -0500

    Cleaning up definition files.

--- khamr/tools/fused_expanse.py
+++ khamr/tools/fused_expanse.py
@@ -4,9 +4,13 @@ import khamr
 from abjad import rhythmmakertools as rhythmos
 
 
-def fused_expanse(fuse_by_counts):
+def fused_expanse(fuse_by_counts, division_mask=None):
     r'''Makes fused expanse rhythm.
     '''
+    if division_mask is None:
+        division_masks = None
+    else:
+        division_masks = [division_mask]
     return baca.RhythmCommand(
         division_maker=khamr.beat_divisions()
             .fuse_by_counts(
@@ -14,6 +18,7 @@ def fused_expanse(fuse_by_counts):
                 ),
         rewrite_meter=True,
         rhythm_maker=rhythmos.NoteRhythmMaker(
+            division_masks=division_masks,
             tie_specifier=rhythmos.TieSpecifier(
                 repeat_ties=True,
                 )
