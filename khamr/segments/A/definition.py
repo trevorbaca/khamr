c075fab21bd262b4ebd4c3a405941420889b7b0b
Trevor Bača <trevor.baca@gmail.com>
2018-03-26 11:42:48 -0500

    Cleaning up definition files.

--- khamr/segments/A/definition.py
+++ khamr/segments/A/definition.py
@@ -114,31 +114,11 @@ maker(
 
 maker(
     ('perc', 4),
-    baca.RhythmCommand(
-        division_maker=baca.DivisionMaker()
-            .split_by_durations(
-                compound_meter_multiplier=abjad.Multiplier(3, 2),
-                durations=[(1, 4)],
-                )
-            .flatten(depth=-1),
-        rewrite_meter=True,
-        rhythm_maker=rhythmos.NoteRhythmMaker(
-            division_masks=[
-                rhythmos.SilenceMask(
-                    pattern=abjad.index([
-                        0, 1, 2, 3,
-                        5,
-                        6, 7, 8, 9, 10,
-                        12,
-                        13, 14, 15, 16,
-                        18, 19, 20,
-                        ]),
-                    ),
-                ],
-            tie_specifier=rhythmos.TieSpecifier(
-                repeat_ties=True,
-                ),
-            ),
+    khamr.quarter_hits(
+        abjad.silence([
+            0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 
+            12, 13, 14, 15, 16, 18, 19, 20,
+            ]),
         ),
     )
 
