fd2eb5e4fa96661070c56b94d24796a548552504
Trevor Bača <trevor.baca@gmail.com>
2018-05-29 15:05:12 -0500

    Cleaned up docstrings.

--- khamr/materials/color_trill_pitches/definition.py
+++ khamr/materials/color_trill_pitches/definition.py
@@ -2,9 +2,9 @@ import abjad
 import baca
 
 
-color_trill_pitches = r'''
+color_trill_pitches = r"""
     F4 F#4 F+4 F4 F+4 F4 E4 E+4 E4 E+4 F4 F+4 F#4 F+4 F#4 F#+4 F#4 E+4
-    '''
+    """
 color_trill_pitches = color_trill_pitches.split()
 color_trill_pitches = [abjad.NamedPitch(_) for _ in color_trill_pitches]
 color_trill_pitches = baca.PitchSegment(
