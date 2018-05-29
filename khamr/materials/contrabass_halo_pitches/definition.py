b64e55a06d6b606ddb45864aabbd97b6c80b5435
Trevor Bača <trevor.baca@gmail.com>
2018-05-29 15:05:12 -0500

    Cleaned up docstrings.

--- khamr/materials/contrabass_halo_pitches/definition.py
+++ khamr/materials/contrabass_halo_pitches/definition.py
@@ -1,11 +1,11 @@
 import abjad
 
 
-contrabass_halo_pitches = r'''
+contrabass_halo_pitches = r"""
     A2 Bb2 A+2 B2 C+3 Bb2 A+2 B2
     A2 B2 C#3 B+2 C3 A2 B2 C#3 B+2 D~3 C3 D3
     A2 D3 C+3 D+3 E3 C#3 D3 E~3 F3 D3 E~3 F~3 E3
-    '''
+    """
 
 contrabass_halo_pitches = contrabass_halo_pitches.split()
 #raise Exception(len(contrabass_halo_pitches))
