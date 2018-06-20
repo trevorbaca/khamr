8d369d0baaa5ec0361730831a81727b11e0bfafe
Trevor Bača <trevor.baca@gmail.com>
2018-06-20 14:25:26 -0500

    Ported helianthation.

--- khamr/materials/time_signatures/definition.py
+++ khamr/materials/time_signatures/definition.py
@@ -7,16 +7,12 @@ import baca
 #   = 151.2 beats at 84 MM
 #   = 113.4 beats at 63 MM
 #   = 75.6 beats at 42 MM
-numerators = baca.helianthate(
-    [[2, 2, 3], [2, 4], [3, 4, 5]],
-    -1,
-    -1
-    )
-pairs = baca.helianthate(
-    [[(2, 4), (2, 4), (6, 4)], [(3, 4), (4, 4)], [(6, 8), (4, 4), (5, 4)]],
-    -1,
-    -1
-    )
-pairs = baca.sequence(pairs).flatten()
+numerators = baca.sequence([[2, 2, 3], [2, 4], [3, 4, 5]])
+numerators = numerators.helianthate(-1, -1)
+pairs = baca.sequence([
+    [(2, 4), (2, 4), (6, 4)], [(3, 4), (4, 4)], [(6, 8), (4, 4), (5, 4)],
+    ])
+pairs = pairs.helianthate(-1, -1)
+pairs = pairs.flatten()
 time_signatures = [abjad.TimeSignature(_) for _ in pairs]
 time_signatures = abjad.CyclicTuple(time_signatures)
