8fa5c417d95f26a316fa3f8ecc2a6f9dc56ff3f0
Trevor Bača <trevor.baca@gmail.com>
2018-07-08 13:46:45 -0500

    Cleaned up markup.

--- khamr/segments/C/definition.py
+++ khamr/segments/C/definition.py
@@ -159,7 +159,7 @@ maker(
 maker(
     ('perc', (4, 7)),
     baca.dynamic('fff'),
-    baca.markups.snare_drum().boxed(),
+    baca.markuplib.snare_drum().boxed(),
     baca.stem_tremolo(selector=baca.pleaves()),
     )
 
@@ -195,12 +195,12 @@ maker(
 
 maker(
     (['vn', 'va'], (1, 7)),
-    baca.markups.bow_on_wooden_mute(),
+    baca.markuplib.bow_on_wooden_mute(),
     )
 
 maker(
     (['vc', 'cb'], (1, 7)),
-    baca.markups.bow_on_tailpiece(),
+    baca.markuplib.bow_on_tailpiece(),
     )
 
 maker(
