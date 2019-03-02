0ba717f11ba256ce776bd8198cee066eedc44011
Trevor Bača <trevor.baca@gmail.com>
2019-03-02 13:32:32 -0600

    Cleaned up typehints.

--- khamr/materials/color_trill_pitches/definition.py
+++ khamr/materials/color_trill_pitches/definition.py
@@ -2,13 +2,13 @@ import abjad
 import baca
 
 
-color_trill_pitches = r"""
+string = r"""
     F4 F#4 F+4 F4 F+4 F4 E4 E+4 E4 E+4 F4 F+4 F#4 F+4 F#4 F#+4 F#4 E+4
     """
-color_trill_pitches = color_trill_pitches.split()
-color_trill_pitches = [abjad.NamedPitch(_) for _ in color_trill_pitches]
+strings = string.split()
+named_pitches = [abjad.NamedPitch(_) for _ in strings]
 color_trill_pitches = baca.PitchSegment(
-    color_trill_pitches, 
+    named_pitches, 
     abjad.NamedPitch,
     )
 assert len(color_trill_pitches) == 18
