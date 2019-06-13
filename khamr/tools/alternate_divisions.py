32d1ee0b6db14fb999badbe90088452f44470892
Trevor Bača <trevor.baca@gmail.com>
2019-06-13 14:22:53 -0400

    Cleaned up tie handling.

--- khamr/tools/alternate_divisions.py
+++ khamr/tools/alternate_divisions.py
@@ -3,19 +3,28 @@ import baca
 from abjadext import rmakers
 
 
-def alternate_divisions(strip_ties: bool = None) -> baca.RhythmCommand:
+def alternate_divisions(detach_ties: bool = None) -> baca.RhythmCommand:
     """
     Makes repeat-tied alternate divisions.
     """
+    specifiers = []
+    if detach_ties is True:
+        specifier = rmakers.TieSpecifier(
+            detach_ties=True, selector=baca.notes()
+        )
+        specifiers.append(specifier)
+        specifier = rmakers.TieSpecifier(
+            detach_repeat_ties=True, selector=baca.notes()
+        )
+        specifiers.append(specifier)
     return baca.rhythm(
         rewrite_meter=True,
         rhythm_maker=rmakers.NoteRhythmMaker(
+            *specifiers,
             beam_specifier=rmakers.BeamSpecifier(beam_each_division=True),
             division_masks=[rmakers.silence([1], 2)],
             tie_specifier=rmakers.TieSpecifier(
-                repeat_ties=True,
-                strip_ties=strip_ties,
-                tie_across_divisions=True,
+                repeat_ties=True, tie_across_divisions=True
             ),
             tag="khamr.alternate_divisions",
         ),
