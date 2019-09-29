809fdc4799c45d31014727e28aa1d91bd248ec4f
Trevor Bača <trevor.baca@gmail.com>
2019-09-29 17:07:15 -0400

    Cleaned up bar lines.

--- khamr/segments/C/definition.py
+++ khamr/segments/C/definition.py
@@ -39,6 +39,7 @@ maker(
     baca.metronome_mark(baca.Accelerando(), selector=baca.leaf(8 - 1)),
     baca.metronome_mark("84", selector=baca.leaf(16 - 1)),
     baca.rehearsal_mark("C"),
+    baca.bar_line("|.", baca.skip(-1)),
 )
 
 maker(
