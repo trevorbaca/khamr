0614ec8a3cda05c47826b5e48dfc3e776245dc66
Trevor Bača <trevor.baca@gmail.com>
2020-05-25 14:24:51 -0400

    Moved materials into tools.py module.

--- khamr/materials/violin_halo_pitches/definition.py
+++
@@ -1,7 +0,0 @@
-import abjad
-from khamr.materials.contrabass_halo_pitches.definition import contrabass_halo_pitches
-
-violin_halo_pitches = []
-for halo_pitch in contrabass_halo_pitches:
-    violin_halo_pitch = halo_pitch + abjad.NamedInterval("m14")
-    violin_halo_pitches.append(violin_halo_pitch)
