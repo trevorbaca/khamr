09f94ac10a9160afb31abbc68ea55ecfbd094b58
Trevor Bača <trevor.baca@gmail.com>
2020-05-25 14:51:53 -0400

    Cleaned up typehints.

--- khamr/tools.py
+++ khamr/tools.py
@@ -229,9 +229,9 @@ for halo_pitch in contrabass_halo_pitches:
 double_stop_halo_pitches = []
 for halo_pitch in contrabass_halo_pitches:
     lower_pitch = halo_pitch - abjad.NamedInterval("M9")
-    named_pitches = (lower_pitch, halo_pitch)
+    named_pitches_pair = (lower_pitch, halo_pitch)
     named_pitch_segment = abjad.PitchSegment(
-        items=named_pitches, item_class=abjad.NamedPitch
+        items=named_pitches_pair, item_class=abjad.NamedPitch
     )
     double_stop_halo_pitches.append(named_pitch_segment)
 
