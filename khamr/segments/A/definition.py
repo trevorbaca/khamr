5205789a693bf65749d23a70e2ae6d76bd43ba81
Trevor Bača <trevor.baca@gmail.com>
2020-07-08 16:39:51 -0400

    Cleaned up path handling.

--- khamr/segments/A/definition.py
+++ khamr/segments/A/definition.py
@@ -1,5 +1,3 @@
-import os
-
 import abjad
 import baca
 import khamr
@@ -23,7 +21,7 @@ maker = baca.SegmentMaker(
     ],
     check_all_are_pitched=True,
     ignore_repeat_pitch_classes=True,
-    segment_directory=abjad.Path(os.path.realpath(__file__)).parent,
+    segment_directory=abjad.Path(__file__).resolve().parent,
     stage_markup=stage_markup,
     time_signatures=khamr.time_signatures[:30],
     transpose_score=True,
