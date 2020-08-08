8f64cf88e1cabcd1f9403285702df080aa7c7693
Trevor Bača <trevor.baca@gmail.com>
2020-08-08 16:00:10 -0400

    Cleaned up tags.

--- khamr/segments/_/definition.py
+++ khamr/segments/_/definition.py
@@ -19,7 +19,7 @@ stage_markup = (
 
 maker = baca.SegmentMaker(
     activate=[
-        abjad.tags.LOCAL_MEASURE_NUMBER,
+        baca.tags.LOCAL_MEASURE_NUMBER,
     ],
     check_all_are_pitched=True,
     ignore_repeat_pitch_classes=True,
