cd4536db8366b1b3cfe641a9e86647d16d86f0a3
Trevor Bača <trevor.baca@gmail.com>
2021-07-28 15:31:25 -0400

    Cleaned up setup.py file.

--- setup.py
+++ setup.py
@@ -1,27 +1,18 @@
 #!/usr/bin/env python
 import setuptools
 
-install_requires = [
-    "abjad",
-    "black",
-    "flake8",
-    "isort",
-    "pytest",
-]
-
-keywords = [
-    "abjad",
-    "lilypond",
-    "music composition",
-    "music notation",
-]
-
 if __name__ == "__main__":
     setuptools.setup(
         author="Trevor Bača",
         author_email="trevor.baca@gmail.com",
-        install_requires=install_requires,
-        keywords=", ".join(keywords),
+        install_requires=[
+            "abjad",
+            "black",
+            "flake8",
+            "isort",
+            "pytest",
+        ],
+        keywords="abjad, lilypond, music composition, music notation",
         name="khamr",
         packages=["khamr"],
         platforms="Any",
