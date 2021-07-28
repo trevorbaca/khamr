17a6042481fd8d355dedc0a35b67ce7483fdb96c
Trevor Bača <trevor.baca@gmail.com>
2021-07-28 15:33:47 -0400

    Cleaned up setup.py file.

--- setup.py
+++ setup.py
@@ -16,5 +16,6 @@ if __name__ == "__main__":
         name="khamr",
         packages=["khamr"],
         platforms="Any",
+        python_requires=">=3.9",
         url="https://github.com/trevorbaca/khamr",
     )
