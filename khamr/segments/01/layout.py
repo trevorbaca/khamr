db23cc75fe860f9bc96c2aa471626493cdc28a3d
Trevor Bača <trevor.baca@gmail.com>
2021-08-07 12:08:14 -0400

    Cleaned up layout files.

--- khamr/segments/01/layout.py
+++ khamr/segments/01/layout.py
@@ -7,43 +7,56 @@ first_page_distances = (20, (30, 30, 30, 40), (30, 30, 40), (30, 30, 30, 40))
 
 breaks = baca.breaks(
     baca.page(
-        baca.system(first_page_distances, measure=1, y_offset=60),
+        baca.system(measure=1, y_offset=60, distances=first_page_distances),
+        number=1,
     ),
     baca.page(
-        baca.system(distances, measure=5, y_offset=y_offset),
+        baca.system(measure=5, y_offset=y_offset, distances=distances),
+        number=2,
     ),
     baca.page(
-        baca.system(distances, measure=8, y_offset=y_offset),
+        baca.system(measure=8, y_offset=y_offset, distances=distances),
+        number=3,
     ),
     baca.page(
-        baca.system(distances, measure=11, y_offset=y_offset),
+        baca.system(measure=11, y_offset=y_offset, distances=distances),
+        number=4,
     ),
     baca.page(
-        baca.system(distances, measure=15, y_offset=y_offset),
+        baca.system(measure=15, y_offset=y_offset, distances=distances),
+        number=5,
     ),
     baca.page(
-        baca.system(distances, measure=18, y_offset=y_offset),
+        baca.system(measure=18, y_offset=y_offset, distances=distances),
+        number=6,
     ),
     baca.page(
-        baca.system(distances, measure=22, y_offset=y_offset),
+        baca.system(measure=22, y_offset=y_offset, distances=distances),
+        number=7,
     ),
     baca.page(
-        baca.system(distances, measure=27, y_offset=y_offset),
+        baca.system(measure=27, y_offset=y_offset, distances=distances),
+        number=8,
     ),
     baca.page(
-        baca.system(distances, measure=30, y_offset=y_offset),
+        baca.system(measure=30, y_offset=y_offset, distances=distances),
+        number=9,
     ),
     baca.page(
-        baca.system(distances, measure=34, y_offset=y_offset),
+        baca.system(measure=34, y_offset=y_offset, distances=distances),
+        number=10,
     ),
     baca.page(
-        baca.system(distances, measure=37, y_offset=y_offset),
+        baca.system(measure=37, y_offset=y_offset, distances=distances),
+        number=11,
     ),
     baca.page(
-        baca.system(distances, measure=41, y_offset=y_offset),
+        baca.system(measure=41, y_offset=y_offset, distances=distances),
+        number=12,
     ),
     baca.page(
-        baca.system(distances, measure=44, y_offset=y_offset),
+        baca.system(measure=44, y_offset=y_offset, distances=distances),
+        number=13,
     ),
 )
 
--- khamr/segments/04/layout.py
+++ khamr/segments/04/layout.py
@@ -5,22 +5,28 @@ distances = (20, (30, 30, 30, 40), (30, 30, 40), (30, 30, 30, 40))
 
 breaks = baca.breaks(
     baca.page(
-        baca.system(distances, measure=1, y_offset=y_offset),
+        baca.system(measure=1, y_offset=y_offset, distances=distances),
+        number=1,
     ),
     baca.page(
-        baca.system(distances, measure=5, y_offset=y_offset),
+        baca.system(measure=5, y_offset=y_offset, distances=distances),
+        number=2,
     ),
     baca.page(
-        baca.system(distances, measure=8, y_offset=y_offset),
+        baca.system(measure=8, y_offset=y_offset, distances=distances),
+        number=3,
     ),
     baca.page(
-        baca.system(distances, measure=11, y_offset=y_offset),
+        baca.system(measure=11, y_offset=y_offset, distances=distances),
+        number=4,
     ),
     baca.page(
-        baca.system(distances, measure=15, y_offset=y_offset),
+        baca.system(measure=15, y_offset=y_offset, distances=distances),
+        number=5,
     ),
     baca.page(
-        baca.system(distances, measure=18, y_offset=y_offset),
+        baca.system(measure=18, y_offset=y_offset, distances=distances),
+        number=6,
     ),
 )
 
