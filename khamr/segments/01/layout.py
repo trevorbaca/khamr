09b687ca0a41702003f303ff37f115b8ad6428bf
Trevor Bača <trevor.baca@gmail.com>
2021-08-09 16:42:19 -0400

    Cleaned up layout files.

--- khamr/segments/01/layout.py
+++ khamr/segments/01/layout.py
@@ -61,7 +61,6 @@ breaks = baca.breaks(
 )
 
 spacing = baca.spacing(
-    __file__,
     breaks=breaks,
     fallback_duration=(1, 32),
 )
--- khamr/segments/02/layout.py
+++ khamr/segments/02/layout.py
@@ -43,7 +43,6 @@ breaks = baca.breaks(
 )
 
 spacing = baca.spacing(
-    __file__,
     breaks=breaks,
     fallback_duration=(1, 32),
 )
