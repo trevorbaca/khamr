2109e062f5d3842d49ab7f22e2d5f14320e1b357
Trevor Bača <trevor.baca@gmail.com>
2021-08-09 20:36:03 -0400

    Cleaned up layout files.

--- khamr/segments/03/layout.py
+++ khamr/segments/03/layout.py
@@ -50,10 +50,5 @@ breaks = baca.breaks(
     ),
 )
 
-spacing = baca.spacing(
-    breaks=breaks,
-    fallback_duration=(1, 32),
-)
-
 if __name__ == "__main__":
-    baca.build.make_layout_ly(breaks, spacing)
+    baca.build.make_layout_ly(breaks, fallback_duration=(1, 32))
