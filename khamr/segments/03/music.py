e0377675633332004f4f2a023e557f46ab0bc529
Trevor Bača <trevor.baca@gmail.com>
2021-09-24 12:23:27 +0200

    Cleaned up definition files.

--- khamr/segments/03/music.py
+++ khamr/segments/03/music.py
@@ -21,9 +21,6 @@ stage_markup = (
 
 maker = baca.SegmentMaker(
     **baca.segments(),
-    activate=[
-        baca.tags.LOCAL_MEASURE_NUMBER,
-    ],
     error_on_not_yet_pitched=True,
     ignore_repeat_pitch_classes=True,
     instruments=khamr.instruments,
@@ -499,4 +496,10 @@ maker(
 )
 
 if __name__ == "__main__":
-    baca.build.make_segment_pdf(maker, runtime=baca.segments(runtime=True))
+    baca.build.make_segment_pdf(
+        maker,
+        **baca.segments(runtime=True),
+        activate=[
+            baca.tags.LOCAL_MEASURE_NUMBER,
+        ],
+    )
--- khamr/segments/04/music.py
+++ khamr/segments/04/music.py
@@ -21,9 +21,6 @@ stage_markup = (
 
 maker = baca.SegmentMaker(
     **baca.segments(),
-    activate=[
-        baca.tags.LOCAL_MEASURE_NUMBER,
-    ],
     error_on_not_yet_pitched=True,
     final_segment=True,
     ignore_repeat_pitch_classes=True,
@@ -272,4 +269,10 @@ maker(
 )
 
 if __name__ == "__main__":
-    baca.build.make_segment_pdf(maker, runtime=baca.segments(runtime=True))
+    baca.build.make_segment_pdf(
+        maker,
+        **baca.segments(runtime=True),
+        activate=[
+            baca.tags.LOCAL_MEASURE_NUMBER,
+        ],
+    )
