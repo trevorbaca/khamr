bcc5a6d47c470288c006dec7099e407d5caccef6
Trevor Bača <trevor.baca@gmail.com>
2021-10-05 12:18:26 +0200

    Bundled LilyPond file creation keywords.

--- khamr/segments/02/music.py
+++ khamr/segments/02/music.py
@@ -519,12 +519,14 @@ if __name__ == "__main__":
     baca.build.make_segment_pdf(
         commands,
         **baca.segment_interpretation_defaults(),
-        activate=[
-            baca.tags.LOCAL_MEASURE_NUMBER,
-        ],
-        error_on_not_yet_pitched=True,
+        activate=(baca.tags.LOCAL_MEASURE_NUMBER,),
         always_make_global_rests=True,
+        error_on_not_yet_pitched=True,
         global_rests_in_topmost_staff=True,
+        lilypond_file_keywords=baca.make_lilypond_file_dictionary(
+            include_layout_ly=True,
+            includes=["../../stylesheet.ily"],
+        ),
         score=score,
         stage_markup=stage_markup,
         transpose_score=True,
