c2709de0a44f794bbf07a7692589493fd5c3ae7b
Trevor Bača <trevor.baca@gmail.com>
2022-02-12 10:58:06 +0100

    Rerendered segments.

--- khamr/segments/02/music.py
+++ khamr/segments/02/music.py
@@ -379,7 +379,7 @@ commands(
     ("perc", (15, 30)),
     baca.pitch("Ab5"),
     baca.stem_down(),
-    library.MarimbaHitCommand([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
+    library.MarimbaHitCommand(indices=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]),
 )
 
 commands(
