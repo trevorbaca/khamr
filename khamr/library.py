770a361df087bd9daa15e47e505c47b15b1ae086
Trevor Bača <trevor.baca@gmail.com>
2022-03-19 12:10:11 +0100

    Cleaned up formatting.

--- khamr/library.py
+++ khamr/library.py
@@ -644,7 +644,7 @@ voice_abbreviations = {
 
 
 def make_empty_score():
-    tag = baca.site(inspect.currentframe())
+    tag = baca.function_name(inspect.currentframe())
     # GLOBAL CONTEXT
     global_context = baca.score.make_global_context()
 
