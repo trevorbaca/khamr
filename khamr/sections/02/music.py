f28b662dad2ae0e8e1eac12dfccce4440e878ecc
Trevor Bača <trevor.baca@gmail.com>
2022-06-25 11:58:08 -0400

    Cleaned up music.py files.

--- khamr/sections/02/music.py
+++ khamr/sections/02/music.py
@@ -51,7 +51,7 @@ for index, item in (
 ):
     skip = skips[index]
     indicator = commands.metronome_marks.get(item, item)
-    baca.commands._metronome_mark(skip, indicator, manifests)
+    baca.metronome_mark(skip, indicator, manifests)
 
 stage_5_silence_pattern = abjad.index(
     [
--- khamr/sections/03/music.py
+++ khamr/sections/03/music.py
@@ -55,7 +55,7 @@ for index, item in (
 ):
     skip = skips[index]
     indicator = commands.metronome_marks.get(item, item)
-    baca.commands._metronome_mark(skip, indicator, manifests)
+    baca.metronome_mark(skip, indicator, manifests)
 
 
 def leaf_in_each_top_tuplet(n):
