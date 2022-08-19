27ec854289867fb0cd195beb7f48a930d52901e8
Trevor Bača <trevor.baca@gmail.com>
2022-08-19 11:02:32 -0400

    Cleaned up music.py files.

--- khamr/sections/03/music.py
+++ khamr/sections/03/music.py
@@ -28,7 +28,7 @@ accumulator = baca.CommandAccumulator(
     short_instrument_names=library.short_instrument_names,
     metronome_marks=library.metronome_marks,
     time_signatures=library.time_signatures()[:37],
-    voice_abbreviations=library.voice_abbreviations,
+    _voice_abbreviations=library.voice_abbreviations,
     voice_names=voice_names,
 )
 
@@ -453,7 +453,7 @@ def main():
     cache = baca.interpret.cache_leaves(
         score,
         len(accumulator.time_signatures),
-        accumulator.voice_abbreviations,
+        library.voice_abbreviations,
     )
     fl(cache["fl"])
     ob(cache["ob"])
