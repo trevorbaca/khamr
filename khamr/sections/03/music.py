9b1c3dedf047216faf455c00ad46526a6e6b1bd6
Trevor Bača <trevor.baca@gmail.com>
2022-08-26 08:09:49 -0400

    Cleaned up music.py files

--- khamr/sections/03/music.py
+++ khamr/sections/03/music.py
@@ -8,48 +8,40 @@ from khamr import library
 ########################################### 03 ##########################################
 #########################################################################################
 
-score = library.make_empty_score()
-voice_names = baca.accumulator.get_voice_names(score)
 
-accumulator = baca.CommandAccumulator(
-    time_signatures=library.time_signatures()[:37],
-    _voice_abbreviations=library.voice_abbreviations,
-    _voice_names=voice_names,
-)
+def make_empty_score():
+    score = library.make_empty_score()
+    voice_names = baca.accumulator.get_voice_names(score)
+    accumulator = baca.CommandAccumulator(
+        time_signatures=library.time_signatures()[:37],
+        _voice_abbreviations=library.voice_abbreviations,
+        _voice_names=voice_names,
+    )
+    return score, accumulator
 
-baca.interpret.set_up_score(
-    score,
-    accumulator.time_signatures,
-    accumulator,
-    library.manifests,
-    append_anchor_skip=True,
-    always_make_global_rests=True,
-)
 
-skips = score["Skips"]
-
-stage_markup = (
-    ("[B.1]", 1),
-    ("[B.2]", 3),
-    ("[B.3]", 5),
-    ("[B.4]", 8),
-    ("[B.5]", 10),
-    ("[B.6]", 26),
-    ("[B.7]", 30),
-    ("[B.8]", 32),
-    ("[B.9]", 34),
-)
-baca.label_stage_numbers(skips, stage_markup)
-
-for index, item in (
-    (5 - 1, "63"),
-    (5 - 1, baca.Accelerando()),
-    (10 - 1, "126"),
-    (26 - 1, "63"),
-    (30 - 1, "42"),
-):
-    skip = skips[index]
-    baca.metronome_mark_function(skip, item, library.manifests)
+def GLOBALS(skips):
+    stage_markup = (
+        ("[B.1]", 1),
+        ("[B.2]", 3),
+        ("[B.3]", 5),
+        ("[B.4]", 8),
+        ("[B.5]", 10),
+        ("[B.6]", 26),
+        ("[B.7]", 30),
+        ("[B.8]", 32),
+        ("[B.9]", 34),
+    )
+    baca.label_stage_numbers(skips, stage_markup)
+    for index, item in (
+        (5 - 1, "63"),
+        (5 - 1, baca.Accelerando()),
+        (10 - 1, "126"),
+        (26 - 1, "63"),
+        (30 - 1, "42"),
+    ):
+        skip = skips[index]
+        baca.metronome_mark_function(skip, item, library.manifests)
 
 
 def leaf_in_each_top_tuplet(argument, n):
@@ -426,7 +418,19 @@ def composites(cache):
             baca.hairpin_function(o, "pp < f")
 
 
-def make_score():
+def make_score(first_measure_number, previous_persistent_indicators):
+    score, accumulator = make_empty_score()
+    baca.interpret.set_up_score(
+        score,
+        accumulator.time_signatures,
+        accumulator,
+        library.manifests,
+        append_anchor_skip=True,
+        always_make_global_rests=True,
+        first_measure_number=first_measure_number,
+        previous_persistent_indicators=previous_persistent_indicators,
+    )
+    GLOBALS(score["Skips"])
     FL(accumulator.voice("fl"), accumulator)
     OB(accumulator.voice("ob"), accumulator)
     CL(accumulator.voice("cl"), accumulator)
@@ -438,8 +442,6 @@ def make_score():
     VA(accumulator.voice("va"), accumulator)
     VC(accumulator.voice("vc"), accumulator)
     CB(accumulator.voice("cb"), accumulator)
-    previous_persist = baca.previous_persist(__file__)
-    previous_persistent_indicators = previous_persist["persistent_indicators"]
     baca.reapply(
         accumulator.voices(),
         library.manifests,
@@ -459,17 +461,24 @@ def make_score():
     perc(cache["perc"])
     cb(cache)
     composites(cache)
+    return score, accumulator
 
 
 def main():
-    make_score()
+    previous_metadata = baca.previous_metadata(__file__)
+    first_measure_number = previous_metadata["final_measure_number"] + 1
+    previous_persist = baca.previous_persist(__file__)
+    score, accumulator = make_score(
+        first_measure_number, previous_persist["persistent_indicators"]
+    )
     metadata, persist, timing = baca.build.section(
         score,
         library.manifests,
         accumulator.time_signatures,
         **baca.interpret.section_defaults(),
-        activate=(baca.tags.LOCAL_MEASURE_NUMBER,),
+        activate=[baca.tags.LOCAL_MEASURE_NUMBER],
         always_make_global_rests=True,
+        first_measure_number=first_measure_number,
         error_on_not_yet_pitched=True,
         global_rests_in_topmost_staff=True,
         transpose_score=True,
