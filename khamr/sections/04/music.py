fd07f746b0ea280196abe20cb470b83224f4116a
Trevor Bača <trevor.baca@gmail.com>
2022-09-26 08:27:58 -0400

    Cleaned up timing.

--- khamr/sections/04/music.py
+++ khamr/sections/04/music.py
@@ -237,6 +237,7 @@ def composites(cache):
             baca.hairpin(o, "p > ppp")
 
 
+@baca.build.timed
 def make_score(first_measure_number, previous_persistent_indicators):
     score, accumulator = make_empty_score()
     baca.section.set_up_score(
@@ -287,9 +288,11 @@ def make_score(first_measure_number, previous_persistent_indicators):
 
 def main():
     environment = baca.build.read_environment(__file__, baca.build.argv())
+    timing = baca.build.Timing()
     score, accumulator = make_score(
         environment.first_measure_number,
         environment.previous_persist["persistent_indicators"],
+        timing,
     )
     defaults = baca.section.section_defaults()
     del defaults["append_anchor_skip"]
