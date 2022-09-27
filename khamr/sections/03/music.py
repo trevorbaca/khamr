3fd7dc43afb36c680fa7ed69fc1142ea139c618b
Trevor Bača <trevor.baca@gmail.com>
2022-09-27 10:25:47 -0400

    Cleaned up metadata.

--- khamr/sections/03/music.py
+++ khamr/sections/03/music.py
@@ -467,8 +467,8 @@ def main():
     )
     baca.build.persist_lilypond_file(
         lilypond_file,
-        environment.metadata,
-        environment.persist,
+        metadata,
+        persist,
         timing,
         environment.arguments,
     )
