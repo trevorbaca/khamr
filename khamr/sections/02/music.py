d77ff365de06910ea5470c848548553a8b041de3
Trevor Bača <trevor.baca@gmail.com>
2022-10-06 07:36:01 -0400

    Cleaned up tags.

--- khamr/sections/02/music.py
+++ khamr/sections/02/music.py
@@ -450,10 +450,8 @@ def main():
         global_rests_in_topmost_staff=True,
         manifests=library.manifests,
         transpose_score=True,
-        tags=baca.tags.Tags(
-            activate=[baca.tags.LOCAL_MEASURE_NUMBER],
-        ),
     )
+    baca.tags.activate(score, baca.tags.LOCAL_MEASURE_NUMBER)
     lilypond_file = baca.lilypond.file(
         score,
         include_layout_ly=True,
