2eb138d1976cd751ffca9bff16a19dcf4c85a892
Trevor Bača <trevor.baca@gmail.com>
2022-10-07 10:33:04 -0400

    Cleaned up music.py files.

--- khamr/sections/04/music.py
+++ khamr/sections/04/music.py
@@ -291,12 +291,12 @@ def main():
     )
     metadata = baca.section.postprocess_score(
         score,
+        environment,
+        library.manifests,
         always_make_global_rests=True,
-        environment=environment,
         error_on_not_yet_pitched=True,
         final_section=True,
         global_rests_in_topmost_staff=True,
-        manifests=library.manifests,
         transpose_score=True,
     )
     baca.tags.activate(score, baca.tags.LOCAL_MEASURE_NUMBER)
