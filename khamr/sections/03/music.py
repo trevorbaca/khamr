1658549dbad306c2deff8b72d4d2f23b3e3f8c57
Trevor Bača <trevor.baca@gmail.com>
2023-04-02 13:49:16 -0400

    Cleaned up music.py files.

--- khamr/sections/03/music.py
+++ khamr/sections/03/music.py
@@ -403,7 +403,7 @@ def make_score(first_measure_number, previous_persistent_indicators):
     VA(voices("va"), time_signatures)
     VC(voices("vc"), time_signatures)
     CB(voices("cb"), time_signatures)
-    baca.section.reapply(
+    baca.section.reapply_persistent_indicators(
         voices,
         previous_persistent_indicators,
         manifests=library.manifests,
