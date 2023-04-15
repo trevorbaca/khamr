ceb7e558dacc68f61324fcebb197902b8a19ae0f
Trevor Bača <trevor.baca@gmail.com>
2023-04-15 09:44:55 -0400

    Cleaned up metronome marks.

--- khamr/library.py
+++ khamr/library.py
@@ -615,11 +615,11 @@ instruments = {
 
 
 metronome_marks = {
-    "32": abjad.MetronomeMark((1, 4), 32),
-    "42": abjad.MetronomeMark((1, 4), 42),
-    "63": abjad.MetronomeMark((1, 4), 63),
-    "84": abjad.MetronomeMark((1, 4), 84),
-    "126": abjad.MetronomeMark((1, 4), 126),
+    "32": abjad.MetronomeMark(abjad.Duration(1, 4), 32),
+    "42": abjad.MetronomeMark(abjad.Duration(1, 4), 42),
+    "63": abjad.MetronomeMark(abjad.Duration(1, 4), 63),
+    "84": abjad.MetronomeMark(abjad.Duration(1, 4), 84),
+    "126": abjad.MetronomeMark(abjad.Duration(1, 4), 126),
 }
 
 
