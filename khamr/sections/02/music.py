d022729cfa21e277bf8c175792e2aabbc6d687f7
Trevor Bača <trevor.baca@gmail.com>
2024-02-23 10:23:27 -0500

    Cleaned up music.py files.

--- khamr/sections/02/music.py
+++ khamr/sections/02/music.py
@@ -375,7 +375,11 @@ def cb(cache):
         m = cache["cb"]
     with baca.scope(m.get(1, 14)) as o:
         baca.dynamic(o.phead(0), "mf")
-        baca.basic_glissando(o.tleaves(), do_not_untie=True)
+        baca.glissando(
+            o.tleaves(),
+            do_not_hide_middle_note_heads=True,
+            do_not_untie=True,
+        )
         baca.markup(
             o.pleaf(0),
             r"\baca-strings-iii-plus-iv-markup",
