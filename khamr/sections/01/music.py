757d10f89f00ea9f5e9e6d098c9d8933b849c322
Trevor Bača <trevor.baca@gmail.com>
2024-04-01 09:51:29 -0400

    Cleaned up music.py files.

--- khamr/sections/01/music.py
+++ khamr/sections/01/music.py
@@ -595,7 +595,7 @@ def make_layout():
         ),
     )
     spacing = baca.layout.Spacing(
-        default_spacing=(1, 32),
+        default=(1, 32),
     )
     baca.build.write_layout_ly(breaks, spacing)
 
