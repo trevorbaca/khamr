6d6e21480a86f2a8d12de4e72d35c9a8a397ef2f
Trevor Bača <trevor.baca@gmail.com>
2024-06-14 16:07:00 -0400

    Cleaned up music.py files.

--- khamr/sections/01/music.py
+++ khamr/sections/01/music.py
@@ -523,7 +523,7 @@ def persist_score(score, environment):
     baca.section.activate_tags(score, baca.tags.LOCAL_MEASURE_NUMBER)
     lilypond_file = baca.lilypond.file(
         score,
-        include_layout_ly=True,
+        include_layout_ily=True,
         includes=["../stylesheet.ily", "header.ily"],
     )
     baca.build.persist_lilypond_file(
@@ -596,7 +596,7 @@ def make_layout():
     spacing = baca.layout.Spacing(
         default=(1, 32),
     )
-    baca.build.write_layout_ly(breaks, spacing)
+    baca.build.write_layout_ily(breaks, spacing)
 
 
 def main():
