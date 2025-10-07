b5aa41556b52d05166207289a708707304b45310
Trevor Bača <trevor.baca@gmail.com>
2025-10-07 14:32:11 -0400

    Clean up calls to rmakers.extract_trivial()

--- source/khamr/sections/01/music.py
+++ source/khamr/sections/01/music.py
@@ -85,7 +85,7 @@ def GT(voice, time_signatures):
     rmakers.force_note(tuplet, tag=abjad.Tag("GT()"))
     rmakers.tie(tuplet[:-1])
     rmakers.rewrite_sustained(tuplet)
-    rmakers.extract_trivial(tuplet)
+    rmakers.extract_trivial([tuplet])
     music = library.make_guitar_accelerando_rhythm(
         time_signatures(25, 40),
         [2, 1],
